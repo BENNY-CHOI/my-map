d447500836087806a4abc686885efca2ebb61a6d
BENNY-CHOI <whjj08@gmail.com>
2025-02-12 14:33:23 +0900

    refactor: 지도 기능을 MapView 컴포넌트로 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
+import MapView from "./components/MapView";
 
-const position: [number, number] = [37.5665, 126.9780]; // 서울 좌표
-
-function App() {
+export default function App() {
   return (
     <div style={{ height: "100vh", width: "100vw" }}>
-      <MapContainer
-        center={position}
-        zoom={13}
-        style={{ height: "100%", width: "100%" }}
-        attributionControl={false} // 필요에 따라 속성 추가
-      >
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={position}>
-          <Popup>여기는 서울입니다!</Popup>
-        </Marker>
-      </MapContainer>
+      <MapView />
     </div>
   );
 }
-
-export default App;
---
+++ src/components/MapView.tsx
@@ -0,0 +1,20 @@
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+const position: [number, number] = [37.5665, 126.9780]; // 서울 좌표
+
+export default function MapView() {
+  return (
+    <MapContainer
+      center={position}
+      zoom={13}
+      style={{ height: "100%", width: "100%" }}
+      attributionControl={false} // 필요에 따라 속성 추가
+    >
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <Marker position={position}>
+        <Popup>여기는 서울입니다!</Popup>
+      </Marker>
+    </MapContainer>
+  );
+}
